05a6bdf86f0f9f6237cfe2cfd377cd7eeac0abd2
Robodays <robodays@mail.ru>
2021-01-25 12:03:36 +0300

    add 16

---
+++ Skillbox/module_16/16.3. /cmake-build-debug/CMakeFiles/16_3__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "16_3__"
+  "16_3__.pdb"
+  "CMakeFiles/16_3__.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16_3__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
