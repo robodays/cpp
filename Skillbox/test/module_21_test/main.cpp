1e732621a50763c260ef053a15273a7ae955c380
robodays <70164230+robodays@users.noreply.github.com>
2021-02-20 03:11:28 +0300

    add test password

--- Skillbox/test/module_21_test/main.cpp
+++ Skillbox/test/module_21_test/main.cpp
@@ -2,39 +2,17 @@
 #include <string>
 
 
-//static std::string str;
-//dffdf
-void funcCount(int step, int &longStep, int &countStep, int &result) {
-    if (step == countStep) {
-        result++;
-        //std::cout << str << std::endl;
-        return;
-    }
-
-    for (int i = 1; i <= longStep; ++i) {
-        step += i;
-        if (step <= countStep) {
-            //str += std::to_string(i);
-            funcCount(step, longStep, countStep, result);
-            //str = str.substr(0,str.length()-1);
-            step -= i;
-        }
-    }
-}
 
 int main()
 {
-    int longStep;
-    int countStep;
+    long long password = 1234567890;
+    long long i;
+    for ( i = 0; i < 999999999999999; ++i) {
+        if (i == password) {
+            std::cout << "password = " << i;
+        }
+    }
 
-    std::cout << "longStep: " << std::endl;
-    std::cin >> longStep;
-    std::cout << "countStep: " << std::endl;
-    std::cin >> countStep;
-    int result = 0;
-    funcCount(0, longStep, countStep, result);
-
-    std::cout << "result: " << result << std::endl;
 }
 
 
