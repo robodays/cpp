750d8ae56031dee711e4c9e416f0fa9bcda03bef
robodays <robodays@mail.ru>
2021-04-04 01:53:57 +0300

    24_2 update2

--- Skillbox/module_24_Struct/24_2_Data_model_for_village/main.cpp
+++ Skillbox/module_24_Struct/24_2_Data_model_for_village/main.cpp
@@ -11,8 +11,8 @@ enum Rooms {
 
 struct Floors {
     int numFloor;
-    int countRoom;
-    float heightFloat;
+    float heightFloor;
+    int countRooms;
     Rooms room[4] = {UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN};
     float squareRoom[4] = {0};
 };
@@ -37,7 +37,9 @@ struct LandPlots {
 bool isStove();
 bool isObject(const std::string& question);
 int square(const std::string& nameObject);
+float checkEnterNumFloat(float num, float min = 0, float max = 1000000);
 int checkEnterNum(int num, int min = 0, int max = 1000000);
+void printAllLendPlots(std::vector<LandPlots>& vector);
 
 int main() {
     std::cout << "Data model for village" << std::endl;
@@ -56,24 +58,29 @@ int main() {
         for (int j = 0; j < landPlot[i].countHouse; ++j) {
             landPlot[i].house.push_back(Houses());
             landPlot[i].house[j].numHouse = j + 1;
-            std::cout << "Information on the house " << landPlot[i].house[j].numHouse << ": " << std::endl;
+            std::cout << "Information about house No. " << landPlot[i].house[j].numHouse << ": " << std::endl;
             landPlot[i].house[j].isStove = isStove();
-            std::cout << "Enter the number of floors: " << std::endl;
+            std::cout << "Enter the number of floors(1-3): " << std::endl;
             std::cin >> landPlot[i].house[j].countFloor;
             landPlot[i].house[j].countFloor = checkEnterNum(landPlot[i].house[j].countFloor, 1, 3);
             for (int k = 0; k < landPlot[i].house[j].countFloor; ++k) {
                 landPlot[i].house[j].floor.push_back(Floors());
                 landPlot[i].house[j].floor[k].numFloor = k + 1;
-                std::cout << "Information on the floor " << landPlot[i].house[j].floor[k].numFloor << ": " << std::endl;
-
-
-                //TODO
-               // int numFloor;
-               // int countRoom;
-               // float heightFloat;
-               // Rooms room[4] = {UNKNOWN,UNKNOWN,UNKNOWN,UNKNOWN};
-               // float squareRoom[4] = {0};
-
+                std::cout << "Information about floor No. " << landPlot[i].house[j].floor[k].numFloor << ": " << std::endl;
+                std::cout << "Enter height the floor(mm): " << std::endl;
+                std::cin >> landPlot[i].house[j].floor[k].heightFloor;
+                landPlot[i].house[j].floor[k].heightFloor = checkEnterNumFloat(landPlot[i].house[j].floor[k].heightFloor, 0, 5000);
+                std::cout << "Enter the number of rooms(2-4): " << std::endl;
+                std::cin >> landPlot[i].house[j].floor[k].countRooms;
+                landPlot[i].house[j].floor[k].countRooms = checkEnterNum(landPlot[i].house[j].floor[k].countRooms, 2, 4);
+                for (int l = 0; l < landPlot[i].house[j].floor[k].countRooms; ++l) {
+                    std::cout << "Choose name room No " << l+1 << " (BEAD_ROOM=>1, KITCHEN=>2, BATHROOM=>3, CHILD_ROOM=>4, GUEST_ROOM=>5): " << std::endl;
+                    int room;
+                    std::cin >> room;
+                    room = checkEnterNum(room, 1, 5);
+                    landPlot[i].house[j].floor[k].room[l] = static_cast<Rooms>(room);
+                    landPlot[i].house[j].floor[k].squareRoom[l] = square("room");
+                }
             }
         }
         if (isObject("Is there a garage?(yes/no)")) {
@@ -88,25 +95,72 @@ int main() {
         }
     }
 
+    printAllLendPlots(landPlot);
 
-
-    //Вначале работы программы пользователь заполняет данные о посёлке: вводит общее количество участков и далее начинает
-    //детализировать каждый из участков.
-
-    //Для каждого участка сначала заполняется общее количество построек, а затем детализируется каждая постройка, этажи,
-    //комнаты на этажах.
-
-    //По итогам введённых данных вы можете рассчитать некий суммарный параметр, например, процент земли, занимаемой
-    //постройками, к общей площади всего посёлка. Главное в этой задаче не сама операция подсчёта, а структуры данных.
     return 0;
 }
 
+void printAllLendPlots(std::vector<LandPlots>& landPlot) {
+    int squareSum = 0;
+    std::cout << "=========================" << std::endl;
+    std::cout << "Output of all land plots." << std::endl;
+    std::cout << "=========================" << std::endl;
+    std::cout << "Number of land plots in the village: " << landPlot.size() << std::endl;
+    for (int i = 0; i < landPlot.size(); i++){
+        std::cout << "Id land plot: " << landPlot[i].id << std::endl;
+        std::cout << "The number of houses on the land plot: " << landPlot[i].countHouse << std::endl;///----------------------
+        for (int j = 0; j < landPlot[i].house.size(); ++j) {
+            std::cout << "Information about house No. " << landPlot[i].house[j].numHouse << ": " << std::endl;
+            std::cout << "The presence of a stove: " << (landPlot[i].house[j].isStove ? "yes" : "no") << std::endl;
+            std::cout << "Enter the number of floors: " << landPlot[i].house[j].countFloor << std::endl;///----------------------------
+            for (int k = 0; k < landPlot[i].house[j].floor.size(); ++k) {
+                std::cout << "Information about floor No. " << landPlot[i].house[j].floor[k].numFloor << ": " << std::endl;
+                std::cout << "Height the floor(mm): " << landPlot[i].house[j].floor[k].heightFloor << std::endl;
+                std::cout << "The number of rooms: " << landPlot[i].house[j].floor[k].countRooms << std::endl;
+                for (int l = 0; l < landPlot[i].house[j].floor[k].countRooms; ++l) {
+                    std::cout << "Room No " << l+1 << ": ";
+                    if (BEAD_ROOM == landPlot[i].house[j].floor[k].room[l])  std::cout << "BEAD_ROOM" << std::endl;
+                    else if (KITCHEN == landPlot[i].house[j].floor[k].room[l])  std::cout << "KITCHEN" << std::endl;
+                    else if (BATHROOM == landPlot[i].house[j].floor[k].room[l])  std::cout << "BATHROOM" << std::endl;
+                    else if (CHILD_ROOM == landPlot[i].house[j].floor[k].room[l])  std::cout << "CHILD_ROOM" << std::endl;
+                    else if (GUEST_ROOM == landPlot[i].house[j].floor[k].room[l])  std::cout << "GUEST_ROOM" << std::endl;
+                    std::cout << "Square room: " << landPlot[i].house[j].floor[k].squareRoom[l] << std::endl;
+                    squareSum += landPlot[i].house[j].floor[k].squareRoom[l];
+                }
+            }
+            if (landPlot[i].garage != 0) {
+                std::cout << "Square garage: " << landPlot[i].garage << std::endl;
+                squareSum += landPlot[i].garage;
+            }
+            if (landPlot[i].bathhouse != 0) {
+                std::cout << "Square bathhouse: " << landPlot[i].bathhouse << std::endl;
+                std::cout << "The presence of a stove in bathhouse: " << (landPlot[i].isStoveBathhouse ? "yes" : "no") << std::endl;
+                squareSum += landPlot[i].bathhouse;
+            }
+            if (landPlot[i].barn != 0) {
+                std::cout << "Square barn: " << landPlot[i].barn << std::endl;
+                landPlot[i].barn = square("barn");
+                squareSum += landPlot[i].barn;
+            }
+        }
+
+    }
+}
+
 int checkEnterNum(int num, int min, int max) {
-   while (num < min || num > max) {
-       std::cout << "Enter again(> " << min << " and < " << max << "): " << std::endl;
-       std::cin >> num;
-   }
-   return num;
+    while (num < min || num > max) {
+        std::cout << "Enter again(> " << min << " and < " << max << "): " << std::endl;
+        std::cin >> num;
+    }
+    return num;
+}
+
+float checkEnterNumFloat(float num, float min, float max) {
+    while (num < min || num > max) {
+        std::cout << "Enter again(> " << min << " and < " << max << "): " << std::endl;
+        std::cin >> num;
+    }
+    return num;
 }
 
 bool isObject(const std::string& question) {
