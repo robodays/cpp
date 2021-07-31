a316295f749c6fb0b038fa5e37a978f67956faea
robodays <70164230+robodays@users.noreply.github.com>
2021-08-01 02:15:18 +0300

    module 24 task 1 done

--- Skillbox/module_24_Working_with_dates/24_1_Program_time _tracking/main.cpp
+++ Skillbox/module_24_Working_with_dates/24_1_Program_time _tracking/main.cpp
@@ -1,9 +1,98 @@
 #include <iostream>
+#include <ctime>
+#include <string>
+#include <vector>
+
+struct tasks{
+    bool statusTask = true;
+    std::string nameTask = " ";
+    std::time_t beginTask = std::time(nullptr);
+    std::time_t endTask = 0;
+};
+
+void BeginTask(std::vector<tasks> &task);
+
+void StatusTask(std::vector<tasks> &task);
+
+void EndTask(std::vector<tasks> &task);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Program time tracking!" << std::endl;
+
+    std::vector<tasks> task;
+    std::cout << "Input command (begin, end, status, exit): " << std::endl;
+    std::string command;
+    std::cin >> command;
+
+    while (command != "exit") {
+        if (command == "begin") {
+            BeginTask(task);
+         } else if (command == "status") {
+            StatusTask(task);
+        } else if (command == "end") {
+            EndTask(task);
+        }
+        std::cout << "Input command (begin, end, status, exit): " << std::endl;
+        std::cin >> command;
+
+    }
     return 0;
 }
+
+
+void BeginTask(std::vector<tasks> &task) {
+    if (!task.empty() && task.back().statusTask) {
+        task.back().statusTask = false;
+        task.back().endTask = std::time(nullptr);
+        std::cout << "Back task close! " << std::endl;
+    }
+    tasks newTask;
+    std::string inputNameTask;
+    std::cout << "Input task name: " << std::endl;
+    std::cin >> inputNameTask;
+
+    newTask.statusTask = true;
+    newTask.nameTask = inputNameTask;
+    newTask.beginTask = std::time(nullptr);
+    newTask.endTask = 0;
+    task.push_back(newTask);
+
+    std::cout << "Add new task: " << newTask.nameTask << std::endl;
+    std::cout << "  time begin: " << asctime(std::localtime(&newTask.beginTask)) << std::endl;
+}
+
+void EndTask(std::vector<tasks> &task) {
+    if (task.back().statusTask) {
+        task.back().statusTask = false;
+        task.back().endTask = std::time(nullptr);
+        std::cout << "Back task close! " << std::endl;
+    } else {
+        std::cout << "No tasks started! " << std::endl;
+    }
+}
+
+void StatusTask(std::vector<tasks> &task) {
+    std::cout << "Output all task:" << std::endl;
+    for (int i = 0; i < task.size(); ++i) {
+        std::cout << "ID task: " << i << std::endl;
+        std::cout << "  Name task: " << task[i].nameTask << std::endl;
+        std::cout << "  Time begin: " << asctime(std::localtime(&task[i].beginTask)) << std::endl;
+
+        std::time_t timeWorkSec;
+        if (task[i].statusTask){
+            std::cout << "  Status: work" << std::endl;
+            timeWorkSec = std::time(nullptr) - task[i].beginTask;
+        } else {
+            std::cout << "  Time end: " << asctime(std::localtime(&task[i].endTask)) << std::endl;
+            std::cout << "  Status: end" << std::endl;
+            timeWorkSec = task[i].endTask - task[i].beginTask;
+        }
+        std::cout << "  Time work: " << timeWorkSec / 60 / 60 << " hours " << timeWorkSec / 60 % 60 << " minutes "
+                  << timeWorkSec % 60 << " seconds"<< std::endl;
+
+    }
+}
+
 /*
 Задание 1. Реализация программы учёта времени
 Что нужно сделать
--- test/module_24_test/main.cpp
+++ test/module_24_test/main.cpp
@@ -7,9 +7,9 @@ int main() {
 //1
     std::srand(std::time(nullptr));// зерно для генератора случайных чисел
 
-    std::cout << std::time(nullptr) << std::endl;// выводит юникс время в секутдах UTC 1626819063
+    std::cout << std::time(nullptr) << std::endl;// выводит юникс время в секундах UTC 1626819063
 
-    std::time_t t = std::time(nullptr); // возвращает std::time_t и принемает std::time_t
+    std::time_t t = std::time(nullptr); // возвращает std::time_t и принимает std::time_t
 
     std::tm* local = std::localtime(&t); // возвращает структуру, подробная информация о локальном времени
     std::cout << local->tm_hour << std::endl; // выводит часы
@@ -17,7 +17,7 @@ int main() {
 //2
     std::cout << std::put_time(local, "%Y/%m/%d %H:%M:%S") << std::endl; // Вывод даты по формату
 //3
-    std::time_t t3 = std::time(nullptr); // возвращает std::time_t и принемает std::time_t
+    std::time_t t3 = std::time(nullptr); // возвращает std::time_t и принимает std::time_t
     std::tm* local3 = std::localtime(&t3); // возвращает структуру, подробная информация о локальном времени
     std::cin >> std::get_time(local3, "%H:%M"); // ввод даты с клавиатуры
     std::cout << std::asctime(local3) << std::endl;
@@ -27,10 +27,10 @@ int main() {
 //    std::cout << std::asctime(&local3) << std::endl;
 
 //4
-    std::time_t t41 = std::time(nullptr); // возвращает std::time_t и принемает std::time_t
+    std::time_t t41 = std::time(nullptr); // возвращает std::time_t и принимает std::time_t
     int pause;
     std::cin >> pause;
-    std::time_t t42= std::time(nullptr); // возвращает std::time_t и принемает std::time_t
+    std::time_t t42= std::time(nullptr); // возвращает std::time_t и принимает std::time_t
     std::tm* local41 = std::localtime(&t41); // возвращает структуру, подробная информация о локальном времени
     std::cout << std::asctime(local41) << std::endl;
     std::tm* local42 = std::localtime(&t42); // возвращает структуру, подробная информация о локальном времени
