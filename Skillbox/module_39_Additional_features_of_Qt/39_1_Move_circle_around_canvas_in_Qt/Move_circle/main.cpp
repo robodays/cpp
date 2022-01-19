82aa2db6e309ec9ccf96759ea85fa5de9eca8bad
robodays <70164230+robodays@users.noreply.github.com>
2022-01-19 20:05:58 +0300

    module 39 done

---
+++ Skillbox/module_39_Additional_features_of_Qt/39_1_Move_circle_around_canvas_in_Qt/Move_circle/main.cpp
@@ -0,0 +1,22 @@
+#include <QQmlDebuggingEnabler>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlDebuggingEnabler enabler;
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/Move_circle/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
